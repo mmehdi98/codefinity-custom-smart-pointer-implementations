c446bfeafcf5d19b99aaad1abd1fb7460359c232
mmehdi98 <>
2026-08-14 19:13:13 +0000

    Move Semantics for UniquePtr

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@
 
 int main()
 {
-    UniquePtr<int> pointer(new int(10));
-    std::cout << *pointer.get() << std::endl;
+    UniquePtr<int> pointer10(new int(10));
+    UniquePtr<int> pointer20(std::move(pointer10));
+
+    // pointer20 = std::move(pointer10);
+    std::cout << *pointer20.get() << std::endl;
     return 0;
 }
--- uniquePtr.h
+++ uniquePtr.h
@@ -5,9 +5,11 @@ class UniquePtr{
 public:
     explicit UniquePtr(T* ptr);
     UniquePtr(const UniquePtr& other) = delete;
+    UniquePtr(UniquePtr&& other) noexcept;
     ~UniquePtr();
 
     UniquePtr& operator =(const UniquePtr& other) = delete;
+    UniquePtr& operator =(UniquePtr&& other) noexcept;
     T& operator *();
     const T& operator *() const;
     T* operator ->();
@@ -24,7 +26,23 @@ template <typename T>
 UniquePtr<T>::UniquePtr(T* ptr) :ptr_(ptr){}
 
 template <typename T>
+UniquePtr<T>::UniquePtr(UniquePtr&& other) noexcept
+    : ptr_(other.ptr_) {
+    other.ptr_ = nullptr;
+}
+
+template <typename T>
 UniquePtr<T>::~UniquePtr(){ delete ptr_; }
+
+template <typename T>
+UniquePtr<T>& UniquePtr<T>::operator=(UniquePtr&& other) noexcept {
+    if (this != &other) {
+        delete ptr_;
+        ptr_ = other.ptr_;
+        other.ptr_ = nullptr;
+    }
+    return *this;
+}
 
 template <typename T>
 T& UniquePtr<T>::operator*(){
